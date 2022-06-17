1e2e526de5bfb4f202f5cebff414ec08ede0ac85
ml37 <richiemu0416@gmail.com>
2022-06-17 15:48:12 +0900

    update ignore, update py

--- YoutubeAudio.py
+++ YoutubeAudio.py
@@ -6,15 +6,15 @@ import os
 
 root = tkinter.Tk()
 
-def URLClick():
-    print("URL Download Button Clicked") 
+def AudioClick():
+    lbl2.config(text="Audio Downloading...")
+    print("Audio Download Button Clicked") 
     urlInput = txt.get()
     print(urlInput)
     yt = YouTube(urlInput)
     stream = yt.streams.filter(only_audio=True).get_audio_only()
     stream.download()
-    lbl2 = tkinter.Label(root, text="Download Complete", font=("Arial Bold", 20))
-    lbl2.grid(column=0, row=1)
+    lbl2.config(text="Downloaded")
     print("Download Complete")
 
 def chClick():
@@ -24,11 +24,27 @@ def chClick():
     print(channelInput)
     ch = Channel(channelInput)
     print('----')
+    print('walalalalalalaru')
     print(ch)
     print('----')
+    """download all video in channel"""
     print(f'Downloading videos by: {ch.channel_name}')
     for video in ch.videos:
-        video.streams.first().download()
+        print(video)
+        print('-------')
+        video.streams.filter(progressive=True).order_by('resolution').desc().first().download()
+
+def VideoClick():
+    lbl2.config(text="Video Downloading...")
+    print("Video Download Button Clicked")
+    urlInput = txt.get()
+    print(urlInput)
+    yt = YouTube(urlInput)
+    stream = yt.streams.filter(progressive=True).order_by('resolution').desc().first()
+    stream.download()
+    lbl2.config(text="Downloaded")
+    print("Download Complete")
+
     
 
 root.title("Youtube Audio Downloader")
@@ -37,9 +53,13 @@ lbl = tkinter.Label(root, text="URL", font=("Arial Bold", 20))
 lbl.grid(column=0, row=0)
 txt = tkinter.Entry(root, width=50, font=("Arial Bold", 20))
 txt.grid(column=1, row=0)
-btn = tkinter.Button(root, text="Download", font=("Arial Bold", 20), command=URLClick)
+btn = tkinter.Button(root, text="Audio", font=("Arial Bold", 20), command=AudioClick)
 btn.grid(column=2, row=0)
-btn2 = tkinter.Button(root, text='Channel', font=("Arial Bold", 20), command=chClick)
+btn2 = tkinter.Button(root, text="Video", font=("Arial Bold", 20), command=VideoClick)
 btn2.grid(column=3, row=0)
+btn3 = tkinter.Button(root, text='Channel', font=("Arial Bold", 20), command=chClick)
+btn3.grid(column=4, row=0)
+lbl2 = tkinter.Label(root, text="Ready", font=("Arial Bold", 20))
+lbl2.grid(column=0, row=1)
 
 root.mainloop()

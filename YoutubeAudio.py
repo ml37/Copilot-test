9b00f27496e98c041dd3bc48b1ba0b5bf04b80b4
ml37 <richiemu0416@gmail.com>
2022-06-17 14:20:21 +0900

    Update YoutubeAudio.py

--- YoutubeAudio.py
+++ YoutubeAudio.py
@@ -1,20 +1,34 @@
 from pytube import YouTube
+from pytube import Channel
 import ffmpeg
 import tkinter
 import os
 
 root = tkinter.Tk()
 
-def waClick():
-    print("Button Clicked") 
-    url = txt.get()
-    print(url)
-    yt = YouTube(url)
+def URLClick():
+    print("URL Download Button Clicked") 
+    urlInput = txt.get()
+    print(urlInput)
+    yt = YouTube(urlInput)
     stream = yt.streams.filter(only_audio=True).get_audio_only()
     stream.download()
     lbl2 = tkinter.Label(root, text="Download Complete", font=("Arial Bold", 20))
     lbl2.grid(column=0, row=1)
     print("Download Complete")
+
+def chClick():
+    print('Channel Download Button Clicked')
+    channelInput = txt.get()
+    print('----')
+    print(channelInput)
+    ch = Channel(channelInput)
+    print('----')
+    print(ch)
+    print('----')
+    print(f'Downloading videos by: {ch.channel_name}')
+    for video in ch.videos:
+        video.streams.first().download()
     
 
 root.title("Youtube Audio Downloader")
@@ -23,7 +37,9 @@ lbl = tkinter.Label(root, text="URL", font=("Arial Bold", 20))
 lbl.grid(column=0, row=0)
 txt = tkinter.Entry(root, width=50, font=("Arial Bold", 20))
 txt.grid(column=1, row=0)
-btn = tkinter.Button(root, text="Download", font=("Arial Bold", 20), command=waClick)
+btn = tkinter.Button(root, text="Download", font=("Arial Bold", 20), command=URLClick)
 btn.grid(column=2, row=0)
+btn2 = tkinter.Button(root, text='Channel', font=("Arial Bold", 20), command=chClick)
+btn2.grid(column=3, row=0)
 
 root.mainloop()

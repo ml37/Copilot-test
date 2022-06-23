62ed4ce97cc203d58adce13ca2981bc41ffb40b1
ml37 <56399130+ml37@users.noreply.github.com>
2022-06-24 00:31:18 +0900

    Add image show in YoutubeDownloaderPyQt5GUI.py

---
+++ YoutubeDownloaderPyQt5GUI.py
@@ -0,0 +1,51 @@
+import sys
+import urllib.request
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+
+#UI파일 연결
+#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+form_class = uic.loadUiType("Downloader.ui")[0]
+
+#화면을 띄우는데 사용되는 Class 선언
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+        self.btn_loadFromFile.clicked.connect(self.loadImageFromFile)
+        self.btn_loadFromWeb.clicked.connect(self.loadImageFromWeb)
+        
+    def loadImageFromFile(self) :
+        #QPixmap 객체 생성 후 이미지 파일을 이용하여 QPixmap에 사진 데이터 Load하고, Label을 이용하여 화면에 표시
+        self.qPixmapFileVar = QPixmap()
+        self.qPixmapFileVar.load("aa.png")
+        self.qPixmapFileVar = self.qPixmapFileVar.scaledToWidth(600)
+        self.lbl_picture.setPixmap(self.qPixmapFileVar)
+    def loadImageFromWeb(self) :
+
+        #Web에서 Image 정보 로드
+        urlString = "https://avatars1.githubusercontent.com/u/44885477?s=460&v=4"
+        imageFromWeb = urllib.request.urlopen(urlString).read()
+
+        #웹에서 Load한 Image를 이용하여 QPixmap에 사진데이터를 Load하고, Label을 이용하여 화면에 표시
+        self.qPixmapWebVar = QPixmap()
+        self.qPixmapWebVar.loadFromData(imageFromWeb)
+        self.qPixmapWebVar = self.qPixmapWebVar.scaledToWidth(600)
+        self.lbl_picture.setPixmap(self.qPixmapWebVar)
+
+if __name__ == "__main__" :
+    #QApplication : 프로그램을 실행시켜주는 클래스
+    app = QApplication(sys.argv) 
+
+    #WindowClass의 인스턴스 생성
+    myWindow = WindowClass() 
+
+    #프로그램 화면을 보여주는 코드
+    myWindow.show()
+
+    #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+    app.exec_()

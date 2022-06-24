63b389a99dfc8af80dc9412b05246a77af5af797
ml37 <richiemu0416@gmail.com>
2022-06-24 15:01:13 +0900

    move to mashior youue downloader

--- youtubedownload.py
+++
@@ -1,6 +0,0 @@
-from pytube import YouTube
-
-url=input("Enter the url: ")
-stream = YouTube(url).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-stream.download()
-print("Downloaded")
